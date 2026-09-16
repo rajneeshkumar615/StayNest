29a0ab8d28fdaa62073613124325454d66dfe604
Rajneesh Kumar <uchihaitachi0612005@gmail.com>
2026-09-16 20:17:10 +0530

    Change MONGO_URI check to require its presence

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ const secret = sessionSecret || "devsecret";
 // ===============================
 // MONGO CHECK
 // ===============================
-if (!process.env.MONGO_URI) {
+if (process.env.MONGO_URI) {
   throw new Error("MONGO_URI is missing in environment variables");
 }
 if (!process.env.SECRET) {
